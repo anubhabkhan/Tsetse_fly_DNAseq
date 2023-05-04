90e329118e45d7bc29ade69ff187ee9f4c138dd8
anubhabkhan <anubhabkhan@gmail.com>
2023-05-04 15:15:33 +0100

    Update demultiplexing_and_trimming.sh

--- demultiplexing_and_trimming.sh
+++ demultiplexing_and_trimming.sh
@@ -20,3 +20,23 @@ do
 n=${file%.*fastq}
 porechop_abi -abi -i ${n}.fastq -o ${n}_porechopped.fastq 
 done
+
+#### STEP 4: Mapping with minimap2
+
+## for mapping to multiple references
+#Edit headers in references for example
+
+less Lox_afr.fna | sed 's/>/>Lox_afr/g' > Lox_afr_edit.fna
+
+#concatenate header edited references with:
+
+cat *.fna >> merged_references.fasta
+
+## map wiht minnimap2
+for file in BC*_porechopped.fastq
+do 
+n=${file%.*fastq}
+minimap2 -a /shared3/Anubhab/Tsetse_flies/ref_dir/genome_subset/merged_ref.fasta ${n}.fastq > ${n}.sam
+done
+
+
